7fd31b99a3ab4c1f2caf5b3c46ebde2b624d55ca
Henry Matheisen <henry.mthsn@gmail.com>
2019-04-12 10:36:34 +0200

    changed error handling

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser"
 import { Watson } from "./services";
-import { Message} from "./models";
+import { Message } from "./models";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/api/message', (req, res) => {
+app.post('/api/message', (req, res, next) => {
     const assistant = new Watson();
     const body: Message = req.body;
 
     assistant.createService()
         .createSession()
         .then(() => assistant.sendMessage(body.input.text))
-        .then(response => res.send(response))
-        .catch(err => res.send(err))
+        .then(response => res.status(200).json(response))
+        .catch(next)
 })
 
 export default app;
